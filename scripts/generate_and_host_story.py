ca84260ebe81fb6297884ee97fdbe1f2b786bcb0
vamit-5 <kukicborislav@gmail.com>
2026-08-23 10:49:27 +0200

    Update generate_and_host_story.py

--- scripts/generate_and_host_story.py
+++ scripts/generate_and_host_story.py
@@ -73,6 +73,10 @@ HIGHLIGHT_WORDS = {
     "tajna", "tajno", "anonimno", "anoniman",
     "ljubav", "strast", "strasti",
     "sada", "odmah", "danas",
+    "garantovano", "garantujemo",
+    "vrele", "vrela",
+    "igre",
+    "jedini", "jedina", "jedinstveno",
 }
 
 # Upiti biraju scene koje prikazuju DISKRETNOST, ANONIMNOST i LJUBAV -
